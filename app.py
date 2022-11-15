0a0f64184051318513feb7ab5196164127a71a19
Erik Enriquez <erik.enriquez01@utrgv.edu>
2022-11-14 19:27:45 -0600

    updated materials for deployment

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("base.html")
+# Function to read in details for page
+def readDetails(filname):
+    with open(filname, 'r') as f:
+        return [line for line in f]
+
+@app.route('/')
+def homePage():
+    name = "Me Myname"
+    details = readDetails('static/details.txt')
+    return render_template("base.html", name=name, aboutMe=details)
 
 @app.route('/user/<name>')
 def greet(name):
     return f'<p>Hello, {name}!</p>'
+
+@app.route('/form', methods=['GET', 'POST'])
+def formDemo():
+    name = None
+    if request.method == 'POST':
+        name = request.form['name']
+
+    return render_template('form.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
