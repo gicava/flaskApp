a8ed375b94206c99127d88a288550ee8daf1e03e
gicava <gerardo.cavazos01@utrgv.edu>
2022-11-17 19:04:45 -0600

    new stuff

--- app.py
+++ app.py
@@ -2,28 +2,26 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Function to read in details for page
-def readDetails(filname):
-    with open(filname, 'r') as f:
+def readDetails(filepath):
+    with open(filepath, 'r') as f:
         return [line for line in f]
 
-@app.route('/')
+@app.route("/")
 def homePage():
-    name = "Me Myname"
+    name = "Gera C"
     details = readDetails('static/details.txt')
-    return render_template("base.html", name=name, aboutMe=details)
+    return render_template('base.html', name=name, aboutMe=details)
 
 @app.route('/user/<name>')
 def greet(name):
     return f'<p>Hello, {name}!</p>'
 
+
 @app.route('/form', methods=['GET', 'POST'])
 def formDemo():
-    name = None
     if request.method == 'POST':
-        name = request.form['name']
-
+        name=request.form['name']
     return render_template('form.html', name=name)
 
-if __name__ == '__main__':
+if __name__ =='__main__':
     app.run(debug=True)
